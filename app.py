8a17b324303db06f91c2afd8ff341b8bfe9108e4
Santos Odigie <D00233649@student.dkit.ie>
2022-03-04 16:48:06 +0000

    Adding project to Git. So fair I have: -created the database -created sessions -successfully implemented a login logout interface

---
+++ app.py
@@ -0,0 +1,86 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+from flask_mysqldb import MySQL, MySQLdb
+from mysql.connector import connect, Error
+# from pubnub.callbacks import SubscribeCallback
+import MySQLdb.cursors
+import re
+
+app = Flask(__name__)
+app.secret_key = "AzurCam123"
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "azurcam"
+
+mysql = MySQL(app)
+print(mysql)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        if 'email' in request.form and 'password' in request.form:
+            email = request.form['email']
+            password = request.form['password']
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+            cursor.execute('SELECT * FROM accounts WHERE email = %s AND password = %s', (email, password,))
+            account = cursor.fetchone()
+
+            if account is not None:
+                session['loggedin'] = True
+                session['id'] = account['id']
+                session['username'] = account['username']
+                return redirect(url_for('home'))
+            else:
+                msg = "Incorrect username or password"
+    return render_template("index.html", msg=msg)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+
+@app.route('/register')
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+        account = cursor.fetchone()
+
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+]', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+
+    elif request.method == 'POST':
+        msg = 'Please fill out the form!'
+
+    return render_template('register.html', msg=msg)
+
+
+@app.route('/home')
+def home():
+    if 'loggedin' in session:
+        return render_template('home.html', username=session['username'])
+    return redirect(url_for('login'))
+
+
+if __name__ == '__main__':
+    app.run()
